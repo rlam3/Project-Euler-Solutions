f131959d56897112a62586ddd8143ff457bfb194
Raymond Lam <raymondlam1991@gmail.com>
2020-10-30 13:39:06 -0500

    style(p001): fixing styling and intentations

--- python3/p001/test_main.py
+++ python3/p001/test_main.py
@@ -1,13 +1,12 @@
-import pytest
-
 from p001.main import findSum
+
 
 class TestFindSum(object):
     """
     This function is to test main.py's functions
     """
-    def test_findSum(self):
+    def test_findSum_10(self):
         assert findSum([3, 5], 10) == 23
 
-    def test_findSum(self):
-      assert findSum([3, 5], 1000) == 266333
+    def test_findSum_1000(self):
+        assert findSum([3, 5], 1000) == 266333
